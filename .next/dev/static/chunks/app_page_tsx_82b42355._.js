2ce4e4ad3f25b07910c55c1a9a43bbba2d949e26
ckr-redlinedatavault <help.ckrdatapoint@gmail.com>
2026-03-03 23:22:32 +0530

    chore: remove .next from tracking

--- .next/dev/static/chunks/app_page_tsx_82b42355._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__fba36273._.js",
-  "static/chunks/node_modules_a293c96a._.js",
-  "static/chunks/[next]_internal_font_google_1fbba1e0._.css"
-],
-    source: "dynamic"
-});
